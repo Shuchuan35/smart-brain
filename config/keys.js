b2d2eca7459751c62b597fc394cd44626efa9f64
scream <hou.jennifer35@gmail.com>
2019-05-26 12:37:23 -0400

    connect to production database

---
+++ config/keys.js
@@ -0,0 +1,2 @@
+module.exports = "mongodb+srv://titus35:titus35@houtitus-oqo9k.mongodb.net/smartbrain?retryWrites=true";
+
--- server.js
+++ server.js
@@ -3,18 +3,28 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = require('./config/keys');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
-mongoose.connect('mongodb://localhost/smartbrain', { useNewUrlParser: true, useFindAndModify: false });
 
-// app.get('/', (req, res) => {
-//     res.send('This is public folder');
-// })
+//connect to production mongodb
+mongoose.connect(MONGODB_URI || 'mongodb://localhost/smartbrain', {useNewUrlParser: true });
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/smartbrain', { useNewUrlParser: true, useFindAndModify: false });
 
 require('./routes/api-routes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', function(req, res) {
+        res.sendFile(__dirname + '/client/build/index.html');
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
